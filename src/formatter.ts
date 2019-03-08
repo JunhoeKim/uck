ebcc92118333d46a658a9d7008886ffdbedd5c6b
JunhoeKim <jhkim@hcil.snu.ac.kr>
2019-03-08 16:54:11 +0900

    Add the method of trimming korean suffixes below 10000

--- src/formatter.ts
+++ src/formatter.ts
@@ -22,25 +22,31 @@ class Formatter {
     }
 
     format(value: number): string {
-        const specifier = new FormatSpecifier(this.locale);
-        let { width, comma, precision, spacing, type } = specifier;
-        precision = Math.max(0, Math.min(20, precision));
-        let roundedDigits = this.round(value, precision);
+        let { width, comma, precision, spacing, type } = new FormatSpecifier(this.locale);
 
+        let roundedDigits = this.applyPrecision(value, precision);
         let format = '';
+
         switch (type) {
-            case 'b':
             default:
                 format = this.basicFormat(roundedDigits);
+                format = this.trimBelow10000(format);
         }
-
         return format;
     }
 
+    /**
+     * 기본 type b, b+에서 사용되는 formatting,
+     * 일반 숫자에 한글을 붙여준다.
+     */
     private basicFormat(roundedValue: string) {
         let result = '';
+
+        // 정수 부분, 소수 부분을 나눔
         let [integer, decimal] = roundedValue.split('.');
         decimal = decimal || '';
+
+        // 정수 부분 값이 10 미만이라면 아무것도 안한 결과를 뱉음
         if (integer.length <= 1) {
             return roundedValue;
         }
@@ -48,11 +54,12 @@ class Formatter {
         result += integer.charAt(integer.length - 1).replace('0', '');
         result += decimal ? '.' + decimal : '';
         const suffixEntries = Object.entries(suffixToValue);
+
         suffixEntries.forEach((pair, index) => {
             const [suffix, value] = pair;
             const digitCount = Math.log10(value);
-            if (integer.length > digitCount) {
 
+            if (integer.length > digitCount) {
                 if (index < suffixEntries.length - 1) {
                     const fromIndex = integer.length - Math.log10(suffixEntries[index + 1][1]) 
                     const toIndex = integer.length - Math.log10(pair[1]);
@@ -69,17 +76,40 @@ class Formatter {
         return result;
     }
 
-    private round(value: number, precision: number): string {
-        const digitCount = Math.floor(Math.log10(value));
-        const precisionValue = Math.pow(10, precision);
+    /**
+     * Precision에 따라 반올림을 하거나 소수 점을 붙여서 string으로 바꿔준다.
+     * @param value input number value
+     * @param precision precision을 적용할 자릿 수
+     */
+    private applyPrecision(value: number, precision: number) {
+        precision = Math.max(0, Math.min(20, precision));
+        const digitCount = value.toString().length;
         if (precision > digitCount) {
             return value.toString() + '.' + '0'.repeat(precision - digitCount);
-        } 
-        return (Math.round(value / precisionValue) * precisionValue).toString();
+        } else {
+            const precisionValue = Math.pow(10, precision);
+            return (Math.round(value / precisionValue) * precisionValue).toString();
+        }
+    }
+
+    /**
+     * Format string의 만 자리 이하의 한글(십, 백, 천) 을 제거한다.
+     * @param format: 현재 까지 processing 된 format string
+     */
+    private trimBelow10000(format: string): string {
+        const targetIndex = format.indexOf('만');
+
+        if (targetIndex === -1) {
+            return format;
+        } else {
+            let belowFormat = format.substring(targetIndex + 1);
+            belowFormat = belowFormat.replace(/[십백천]/g, '');
+            return format.substring(0, targetIndex + 1) + belowFormat;
+        }
     }
 }
 
-export default function(locale: string) {
+export default function (locale: string) {
     const formatter = new Formatter(locale);
     return formatter.format.bind(formatter);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-// export { default as format } from './locale';
-// export { default as specifier } from './specifiler';
+export { default as format } from './formatter';
+export { default as specifier } from './specifiler';
 
 
-import { default as format } from './locale';
+import { default as format } from './formatter';
 
 console.log(12345678, '', format('')(12345678));
 console.log(12345678, '.4', format('.4')(12345678));
